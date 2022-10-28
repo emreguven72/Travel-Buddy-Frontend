15ea122a5f5d3e4b2ae3e7b66bffdc40e5776028
emreguven72 <emreguven_72@hotmail.com>
2022-10-28 13:22:31 +0300

    Create user and Delete user services are on

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { View } from 'react-native';
 import styled from 'styled-components/native';
 import PressableButton from './components/PressableButton';
 import UserService from './services/userService';
-import TextInput from './components/TextInput';
 
 export default function App() {
 
   const [users, setUsers] = useState([]);
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState();
+  let userService = new UserService();
 
   useEffect(() => {
-    let userService = new UserService();
     userService.getAllUsers().then(result => setUsers(result.data)).catch((error) => {
       console.log(error)
     })
   }, [])
+
+  const createUser = (email) => {
+    userService.createUser(email);
+  }
+
+  const deleteUser = () => {
+    userService.deleteUser(9);
+  }
 
   const Container = styled.View({
     flex: 1,
@@ -25,35 +32,45 @@ export default function App() {
     justifyContent: 'center'
   })
 
+  const FlexRowContainer = styled.View({
+    flexDirection: 'row'
+  })
+
+  const FlexColumnContainer = styled.View({
+    flexDirection: 'column'
+  })
+
   const AppText = styled.Text({
     color: 'red',
     fontSize: 20
   })
 
-  const getInput = () => {
-    console.log(email);
-  }
+  const EmailText = styled.TextInput({
+    marginTop: 20,
+    paddingHorizontal: 10,
+    backgroundColor: 'pink',
+    flexBasis: '70%',
+    height: '80%',
+    borderRadius: 10
+  })
 
   return (
     <Container>      
-      
-
       {users.map((user) => (
         <View key={user.id}>
-          <Text>{user.email}</Text>
-          <Text>{user.username}</Text>
+          <AppText>{user.username}</AppText>
         </View>
       ))}
-
-      <TextInput 
-        placeholder="Email"
-        label="email"
-        value={email}
-        onChangeText={email => setEmail(email)}
-       />
       
-
-      <PressableButton onPress={getInput} title='Button' bgColor='pink' />
+      <FlexRowContainer>
+        <EmailText
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+      </FlexRowContainer>
+      
+      <PressableButton onPress={() => createUser(email)} title='Create User' bgColor='pink' />
+      <PressableButton onPress={() => deleteUser()} title='Delete User' bgColor='pink' />
       <StatusBar style="auto" />
     </Container>
   );
